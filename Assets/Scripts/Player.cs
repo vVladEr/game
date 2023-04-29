b68d51da4eeaa46acab196757cbc92ca7c2f8126
Stepan Kuchin <stepa.ku4in@gmail.com>
2023-04-29 21:40:20 +0500

    refactor player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 namespace Game 
 {
@@ -7,44 +8,46 @@ namespace Game
         [SerializeField] public Shadow rightShadow;
         [SerializeField] public Shadow topShadow;
         [SerializeField] public Shadow bottomShadow;
+        public IReadOnlyDictionary<Vector2, Shadow> shadowsDictionary;
         public Vector2 InitialPosition = new Vector2(0.5f, 0.5f);
         public Vector2 PreviousPosition;
-        // Start is called before the first frame update
+
         void Start()
         {
             transform.position = InitialPosition;
+            shadowsDictionary = new Dictionary<Vector2, Shadow>() { {new Vector2(1, 0), rightShadow},
+                {new Vector2(-1, 0), leftShadow}, {new Vector2(0, 1), topShadow}, {new Vector2(0, -1), bottomShadow} };
         }
 
-        // Update is called once per frame
         void Update()
         {
             Vector2 currentPosition = transform.position;
             Vector2 newPosition = currentPosition;
-            if (Input.GetKeyDown(KeyCode.D) && rightShadow.NotCollided)
+            if (Input.anyKeyDown)
             {
-                PreviousPosition = currentPosition;
-                newPosition = new(currentPosition.x + 1f, currentPosition.y);
+                var directionVector = GetDirectionVector();
+                if (directionVector != new Vector2(0, 0) && shadowsDictionary[directionVector].NotCollided)
+                {
+                    PreviousPosition = currentPosition;
+                    newPosition = currentPosition + directionVector;
+                }
             }
 
-            if (Input.GetKeyDown(KeyCode.A) && leftShadow.NotCollided)
-            {
-                PreviousPosition = currentPosition;
-                newPosition = new(currentPosition.x - 1f, currentPosition.y);
-            }
-
-            if (Input.GetKeyDown(KeyCode.W) && topShadow.NotCollided)
-            {
-                PreviousPosition = currentPosition;
-                newPosition = new(currentPosition.x, currentPosition.y + 1f);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S) && bottomShadow.NotCollided)
-            {
-                PreviousPosition = currentPosition;
-                newPosition = new(currentPosition.x, currentPosition.y - 1f);
-            }
             transform.position = newPosition;
+        }
+
+        Vector2 GetDirectionVector()
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+                return new Vector2(1, 0);
+            if (Input.GetKeyDown(KeyCode.A))
+                return new Vector2(-1, 0);
+            if (Input.GetKeyDown(KeyCode.W))
+                return new Vector2(0, 1);
+            if (Input.GetKeyDown(KeyCode.S))
+                return new Vector2(0, -1);
+
+            return new Vector2(0, 0);
         }
     }
 }
-
