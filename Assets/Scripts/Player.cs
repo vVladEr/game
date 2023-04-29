9b15a0e7183ffb0cbba8ebab4e4434c483e4ad44
vVladEr <vlader.2004.ekb@gmail.com>
2023-04-29 19:26:44 +0500

    fix collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,12 @@ namespace Game
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] public Shadow shadow;
+        [SerializeField] public Shadow leftShadow;
+        [SerializeField] public Shadow rightShadow;
+        [SerializeField] public Shadow topShadow;
+        [SerializeField] public Shadow bottomShadow;
         public Vector2 InitialPosition = new Vector2(0.5f, 0.5f);
+        public Vector2 PreviousPosition;
         // Start is called before the first frame update
         void Start()
         {
@@ -14,8 +18,32 @@ namespace Game
         // Update is called once per frame
         void Update()
         {
-            transform.position = shadow.CurrentPosition;
-            Debug.Log("move Player");
+            Vector2 currentPosition = transform.position;
+            Vector2 newPosition = currentPosition;
+            if (Input.GetKeyDown(KeyCode.D) && rightShadow.NotCollided)
+            {
+                PreviousPosition = currentPosition;
+                newPosition = new(currentPosition.x + 1f, currentPosition.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) && leftShadow.NotCollided)
+            {
+                PreviousPosition = currentPosition;
+                newPosition = new(currentPosition.x - 1f, currentPosition.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.W) && topShadow.NotCollided)
+            {
+                PreviousPosition = currentPosition;
+                newPosition = new(currentPosition.x, currentPosition.y + 1f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.S) && bottomShadow.NotCollided)
+            {
+                PreviousPosition = currentPosition;
+                newPosition = new(currentPosition.x, currentPosition.y - 1f);
+            }
+            transform.position = newPosition;
         }
     }
 }
--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -6,57 +6,34 @@ using UnityEngine;
 public class Shadow : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector2 PreviousPosition;
-    public Vector2 CurrentPosition;
-    [SerializeField] public Player player; 
+    [SerializeField] public Player player;
+    public bool NotCollided = true;
+    public float dx;
+    public float dy;
     void Start()
     {
-        transform.position = player.InitialPosition;
-        PreviousPosition = transform.position;
-
+        transform.position = new Vector2(player.InitialPosition.x +dx,player.InitialPosition.y + dy);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 currentPosition = transform.position;
-        Vector2 newPosition = currentPosition;
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            PreviousPosition = currentPosition;
-            newPosition = new(currentPosition.x + 1f, currentPosition.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            PreviousPosition = currentPosition;
-            newPosition = new(currentPosition.x - 1f, currentPosition.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            PreviousPosition = currentPosition;
-            newPosition = new(currentPosition.x, currentPosition.y + 1f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            PreviousPosition = currentPosition;
-            newPosition = new(currentPosition.x, currentPosition.y - 1f);
-        }
-        transform.position = newPosition;
-        CurrentPosition = newPosition;
+        transform.position = new Vector2(player.transform.position.x + dx, player.transform.position.y + dy);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag != "Player")
+        if (collision.gameObject.tag == "Wall")
         {
-            transform.position = PreviousPosition;
+            NotCollided = false;
         }
-        else 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
         {
-            CurrentPosition = transform.position;
+            NotCollided = true;
         }
     }
 }
