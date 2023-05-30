1e445226573ae424093cb78a6c955e5c80d5f36c
vVladEr <vlader.2004.ekb@gmail.com>
2023-05-30 10:08:44 +0500

    add pause menu

---
+++ Assets/Scripts/PausedMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pause;
+    [SerializeField] private AudioSource pauseSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0;
+            pauseSource.Pause();
+        }
+    }
+
+    public void PauseOff() 
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+        pauseSource.Play();
+    }
+    public void BackToMenu() 
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+}
