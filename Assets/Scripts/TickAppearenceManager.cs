1eee828b74fdc52a6828d399c05d2766cd9eec39
sergey.kheyfets <telegram only>
2023-05-31 01:21:41 +0500

    [pre-final] anvil fix, assets cleaning

--- Assets/Scripts/TickAppearenceManager.cs
+++ Assets/Scripts/TickAppearenceManager.cs
@@ -6,16 +6,39 @@ using UnityEngine.UI;
 public class TickAppearenceManager : MonoBehaviour
 {
     [SerializeField] private Player player;
-    [SerializeField] private Animator animator;
+    [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _audio; 
+    
+    [SerializeField] private float epsForAnvilAnimation = 0.05f;
+    [SerializeField] private float epsForAnvilSound = 0.05f;
+    private float DeltaTime => 60 / General.BPM;
+    [SerializeField] private General General;
+    public bool isSoundPlayed = false;
+    public bool isAnimationPlayed = false;
+    
 
     private void Update()
-    {
-        animator.SetBool("shouldSmash", player.RightTime);
-    }
-
-    public void play_sound()
-    {
-        _audio.Play();
+    {    
+        var currentTime = General.Time;
+        if (currentTime % DeltaTime < epsForAnvilSound)
+            {
+                if (!isSoundPlayed)
+                {
+                    _audio.Play();
+                    isSoundPlayed = true;
+                }
+            }
+        else isSoundPlayed = false;
+        
+        currentTime = General.Time;
+        if (currentTime % DeltaTime > DeltaTime - epsForAnvilAnimation)
+            {
+                if (!isAnimationPlayed)
+                {
+                    _animator.Play("anvil_hit");
+                    isAnimationPlayed = true;
+                }
+            }
+        else isAnimationPlayed = false;
     }
 }
